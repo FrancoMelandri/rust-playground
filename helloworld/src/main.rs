3b6c4fd3ec0e021e9a7fd94024bce0615f5fef7e
franco-melandri-skanray <franco.melandri@skanray.com>
2025-09-05 18:00:07 +0200

    formatting

--- helloworld/src/main.rs
+++ helloworld/src/main.rs
@@ -3,7 +3,6 @@ use helloworld::Errors;
 use helloworld::get_args;
 use helloworld::get_greeting;
 
-
 fn main() -> Result<(), Errors> {
     const WRONG_ARG_ID: u32 = 1;
     let args: Arguments = get_args();
