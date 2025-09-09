e3585e4fc8b552f820f215fde636dc5062ceec5e
franco-melandri-skanray <franco.melandri@skanray.com>
2025-09-09 13:31:20 +0200

    introducing stop_watch module

--- helloworld/src/main.rs
+++ helloworld/src/main.rs
@@ -1,16 +1,14 @@
-use helloworld::Arguments;
-use helloworld::Errors;
-use helloworld::get_args;
-use helloworld::get_greeting;
+use helloworld::{Arguments, Errors, Stopwatch, get_args, get_greeting};
 
 fn main() -> Result<(), Errors> {
     const WRONG_ARG_ID: u32 = 1;
+    let sw: Stopwatch = Stopwatch::start();
     let args: Arguments = get_args();
 
     match args.name.is_empty() {
         true => Err(Errors::NoArguments(WRONG_ARG_ID)),
         false => {
-            println!("{}", get_greeting(args));
+            println!("{} in {}", get_greeting(args), sw.stop().elapsed);
             Ok(())
         }
     }
---
+++ helloworld/tests/stop_watch_tests.rs
@@ -0,0 +1,14 @@
+mod stop_watch_tests {
+    use std::{thread::sleep, time::Duration};
+
+    use helloworld::Stopwatch;
+
+    #[test]
+    fn test_times_is_wotking() {
+        let mut times: Stopwatch = Stopwatch::start();
+        assert_eq!(times.elapsed, 0f64);
+        sleep(Duration::from_secs(1));
+        times = times.stop();
+        assert_eq!(times.elapsed > 1000f64 && times.elapsed < 1001f64, true);
+    }
+}
